442a3b66506e7bfe0a74c3b1d17cb21b7212873f
Preston Copeland <copelandps1@gmail.com>
2013-10-07 07:26:15 -0400

    First commit for 617 by Scott

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cosc617app::Application.config.secret_key_base = '6ef750486d2086f86406b73773cbab010fefab7ab67f3b04df2cfe75d39ad0b4f4ec9bb6b8686bf839175058fbe8321d22310d7b40e700ea101c3ee13307683c'
